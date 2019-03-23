815cf49ceb0599ae78a9b01eb34d0247da37ab72
유성훈 <loisikr@naver.com>
2019-03-24 03:45:00 +0900

    새로운 과제(Example4) 완료

--- Example4/Example4.cpp
+++ Example4/Example4.cpp
@@ -1,18 +1,89 @@
 ﻿#include <CoalaMOD.h>
 #pragma comment(lib, "CoalaMOD.lib")
+#include <math.h>
 
 int main()
 {
-    std::cout << "Hello World!\n"; 
+	int px, py, pz;
+	getPlayerLocation(&px, &py, &pz);  // 플레이어의 위치값 얻기
+
+	double dir = getPlayerDirection(); // 플레이어의 방향값 얻기
+
+	double pi = 3.14159265358979323846;
+	double radian = dir * pi / 180.;
+
+	int dx = -10 * sin(radian);
+	int dz =  10 * cos(radian);
+
+	
+	px += dx;
+	pz += dz - 1;
+
+	BlockID my_emerald = createBlock(BLOCK_EMERALD);
+	BlockID my_obsidian = createBlock(BLOCK_OBSIDIAN);
+
+	for (int h = 0; h < 5; h++) // 1층 ~ 5층
+	{
+		for (int i = -2; i < 3; i++)
+		{
+			for (int j = -2; j < 3; j++)
+			{
+				if (abs(i) == 2 || abs(j) == 2)
+				{
+					if (abs(i) == 2 && abs(j) == 2)
+					{
+						continue;
+					}
+
+					else
+					{
+						locateBlock(my_emerald, px + i, py + h, pz + j);
+						locateBlock(my_emerald, px + j, py + h, pz + i);
+					}
+				}
+
+				else
+				{
+					continue;
+				}
+			}
+		}
+
+	}
+
+
+	for (int h = 5; h < 7; h++) // 6층 ~ 7층
+	{
+		for (int i = -3; i < 4; i++)
+		{
+			for (int j = -3; j < 4; j++)
+			{
+				if (abs(i) * abs(j) < 5)
+				{
+					if (abs(i) * abs(j) == 4)
+					{
+						locateBlock(my_emerald, px + i, py + h, pz + j);
+					}
+
+					else if (abs(i) == 3 || abs(j) == 3)
+					{
+						locateBlock(my_emerald, px + i, py + h, pz + j);
+						locateBlock(my_emerald, px + j, py + h, pz + i);
+					}
+
+					else if (h == 5 && abs(i) * abs(j) <= 2)
+					{
+						locateBlock(my_obsidian, px + i, py + h, pz + j);
+						locateBlock(my_obsidian, px + j, py + h, pz + i);
+					}
+				}
+
+				else
+				{
+					continue;
+				}
+			}
+		}
+	}
+
 }
-
-// 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
-// 프로그램 디버그: <F5> 키 또는 [디버그] > [디버깅 시작] 메뉴
-
-// 시작을 위한 팁: 
-//   1. [솔루션 탐색기] 창을 사용하여 파일을 추가/관리합니다.
-//   2. [팀 탐색기] 창을 사용하여 소스 제어에 연결합니다.
-//   3. [출력] 창을 사용하여 빌드 출력 및 기타 메시지를 확인합니다.
-//   4. [오류 목록] 창을 사용하여 오류를 봅니다.
-//   5. [프로젝트] > [새 항목 추가]로 이동하여 새 코드 파일을 만들거나, [프로젝트] > [기존 항목 추가]로 이동하여 기존 코드 파일을 프로젝트에 추가합니다.
-//   6. 나중에 이 프로젝트를 다시 열려면 [파일] > [열기] > [프로젝트]로 이동하고 .sln 파일을 선택합니다.
